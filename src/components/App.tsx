5a0d9a7a752195b73702f3868b499bbdece85e18
Malik Elgabroun <malikgabroun@yahoo.co.uk>
2022-05-20 03:58:45 +0100

    inital commit

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: red;
+`;
+
+const App = () => {
+  return (
+    <div>
+      <Title>hello world!!</Title>
+    </div>
+  );
+};
+
+export default App;
